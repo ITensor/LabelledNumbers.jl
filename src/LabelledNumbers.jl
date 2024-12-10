5da7ce40ee9135140b0d47068073694a8c421b54
Lukas Devos <ldevos98@gmail.com>
2024-12-09 19:00:46 -0500

    move blockarrays to extension

--- src/LabelledNumbers.jl
+++ src/LabelledNumbers.jl
@@ -1,8 +1,9 @@
 module LabelledNumbers
+
 include("labelled_interface.jl")
 include("labellednumber.jl")
 include("labelledinteger.jl")
 include("labelledarray.jl")
 include("labelledunitrange.jl")
-include("LabelledNumbersBlockArraysExt.jl")
+
 end
--- test/test_basics.jl
+++ test/test_basics.jl
@@ -119,18 +119,3 @@ using Test: @test, @testset
     end
   end
 end
-
-using BlockArrays: Block, blockaxes, blocklength, blocklengths
-@testset "LabelledNumbersBlockArraysExt" begin
-  x = labelled(1:2, "x")
-  @test blockaxes(x) == (Block.(1:1),)
-  @test blocklength(x) == 1
-  @test blocklengths(x) == [2]
-  a = x[Block(1)]
-  @test a == 1:2
-  @test a isa LabelledUnitRange
-  @test label(a) == "x"
-  @test label_type(a) === String
-  @test label_type(typeof(a)) === String
-end
-end
